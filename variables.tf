4d752db693ea66421c7b2645be35adc8be16ac0f
Camilo Hurtado <chc201627@gmail.com>
2023-12-04 08:07:54 -0500

    Add third subnet and tags

--- variables.tf
+++ variables.tf
@@ -19,6 +19,10 @@ variable "environment" {
   default     = "development"
 }
 
+variable "billing_code" {
+  type        = string
+  description = "(Required) describe your variable"
+}
 variable "cidr_block" {
   type        = string
   description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
